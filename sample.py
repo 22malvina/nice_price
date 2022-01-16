5ca67b46c98765de00f7790a882ad331047c9917
a.tsurkov <a.tsurkov@MAC932.local>
2022-01-16 17:26:53 +0300

    add new

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 lst = list()
-lst = ['Python', 'Machine Learning', 'R Language', 'Bootstrap', 'New folder']
+lst = ['Python', 'Machine Learning', 'R Language', 'Bootstrap', 'New folder', 'Kak zalit']
 for x in lst:
     print(x)
 # test
