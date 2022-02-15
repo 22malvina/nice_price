9d03c227bc9556b3870d8652926a21851f273368
Egor <bukreevegor7@gmail.com>
2022-02-15 21:39:21 +0300

    work_with_json

---
+++ work_with_json.py
@@ -0,0 +1,13 @@
+import json # импорт библиотеки
+
+#with open("data_file.json", "w") as write_file: # открываем фаил на запись
+    #json.dump(data, write_file) # записываем переменную data преобразованную в json в фаил write_file 
+
+#json_string = json.dumps(data) # созадаем строковую переменную содержащую текс в офрмате json из переменной data
+
+with open("test_json.txt", "r") as read_file: 
+    data = json.load(read_file)
+    print(data)
+
+#data = json.loads(json_string)
+
