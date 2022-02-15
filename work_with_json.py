334b6c607c0fb2cba3beb94c6e77737c50fea26f
Puzinator <dimbukreef@yandex.ru>
2022-02-15 22:02:22 +0300

    добавил сохранения в файл

--- work_with_json.py
+++ work_with_json.py
@@ -8,6 +8,11 @@ import json # импорт библиотеки
 with open("test_json.txt", "r") as read_file: 
     data = json.load(read_file)
     print(data)
+    data['president']['year'] = data['president']['year'] + 10
+    print(data)
+    
 
 #data = json.loads(json_string)
 
+with open("data_file.json", "w") as write_file: # открываем фаил на запись
+    json.dump(data, write_file)
