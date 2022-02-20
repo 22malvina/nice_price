0081d1410322ac7238e08cab2d60cbc0e2c74299
Puzinator <dimbukreef@yandex.ru>
2022-02-20 22:13:45 +0300

    new files

---
+++ src/Dima/work_with_json.py
@@ -0,0 +1,29 @@
+import json # импорт библиотеки
+
+#with open("data_file.json", "w") as write_file: # открываем фаил на запись
+    #json.dump(data, write_file) # записываем переменную data преобразованную в json в фаил write_file 
+
+#json_string = json.dumps(data) # созадаем строковую переменную содержащую текс в офрмате json из переменной data
+
+with open("Homework.txt", "r") as read_file: 
+    data = json.load(read_file)
+    print(data)
+    Values = data["Films"].values()
+    x = 0
+    z = 0
+    for i in Values:
+        x += 1
+        z += i
+    
+    y = z / x
+    print(y)
+   # data['president']['year'] = data['president']['year'] + 10
+   # print(data)
+    
+
+#data = json.loads(json_string)
+
+with open("data_file.json", "w") as write_file: # открываем фаил на запись
+    json.dump(data, write_file)
+
+
