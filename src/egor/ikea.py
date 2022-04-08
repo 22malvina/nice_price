faa441d76fc4d43933559367937afac78bd4a439
Egor <bukreevegor7@gmail.com>
2022-04-08 21:17:14 +0300

    'fordima'

--- src/egor/ikea.py
+++ src/egor/ikea.py
@@ -3,26 +3,28 @@ import requests
 import re
 import json
 
+
+
 with open("ikea2.json", "r") as read_file: #Открываем файл с ссылками
-        data = json.load(read_file) #  В переменную доббавляем все содержимое файла      
-        for i in data: # Пробегаемя по файлу
-            n = i["link"] # Достаем значение с ключом link            
-            page = requests.get(n,timeout=5) # Отправляем запрос на  ссылку переменной j          
-            soup=BeautifulSoup(page.text,"html.parser") # Создаем объект
-            #allPrice=soup.findAll('a' )
-            allPrice=soup.findAll('span', class_ = 'pip-price__integer'  )
-            for one_price in allPrice:             
-                links_2.append(one_price.text)
-            print(links_2)
-
-        new_list = []
+    
+    links_3 = []
+            
+    data = json.load(read_file) #  В переменную доббавляем все содержимое файла      
+    for i in data: # Пробегаемя по файлу
+        n = i["link"] # Достаем значение с ключом link            
+        page = requests.get(n,timeout=5) # Отправляем запрос на  ссылку переменной j          
+        soup=BeautifulSoup(page.text,"html.parser") # Создаем объект
+        #allPrice=soup.findAll('a' )
+        allPrice=soup.findAll('span', class_ = 'pip-price__integer'  )
+        print(allPrice)  
+        for one_price in allPrice:
+                    
+            links_3.append(one_price.text)
         
-        for k in links_2:
-            new_list.append({'price':k})
-        new_list_size_100 = new_list[0:100]
-
-with open("ikea2.json", "w") as write_file: # открываем фаил на запись
-    json.dump(new_list_size_100, write_file)
+        
+    
+                
+               
 
 
 #ikea.com    
@@ -129,7 +131,8 @@ def size_100(final_links_list):
     mnozhestva = set(final_links_list)
     new_list = []
     for k in mnozhestva:
-        new_list.append({'link':k})
+        new_list.append({'link':k,
+                         'price':p})
     new_list_size_100 = new_list[0:100]
     return new_list_size_100
 
